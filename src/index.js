d0d5a2f33bf5052793bbb2b5d2861869123525a0
ShustKristina <shust1991@mail.ru>
2018-02-23 14:57:07 +0300

    countLoveTriangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+        var countTriangle=0;
+    for(var i=0; i<preferences.length; i++){
+      var number1=preferences[i];
+      var number2=preferences[number1-1];
+      var number3=preferences[number2-1];
+      if ((i+1) == number3 && (i+1) != number2){
+        countTriangle++;
+          preferences[i]=0;
+          preferences[number1-1]=0;
+          preferences[number2-1]=0;
+      }
+    }
+    return countTriangle;
+  };
