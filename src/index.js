6472b0d00c82433e06479a5faa95bb8571ad5477
ShustKristina <shust1991@mail.ru>
2018-03-20 21:50:46 +0300

    correct

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
       var number2=preferences[number1-1];
       var number3=preferences[number2-1];
       if ((i+1) == number3 && (i+1) != number2){
-        countTriangle++;
-          preferences[i]=0;
-          preferences[number1-1]=0;
-          preferences[number2-1]=0;
+        countTriangle++;    
       }
     }
-    return countTriangle;
+    return countTriangle/3;
   };
